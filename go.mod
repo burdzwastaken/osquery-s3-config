bc6e9077ccecd1591a17c44fa181e1d8476f94ba
Matt Burdan <burdz@burdz.net>
2025-07-12 10:25:08 -0700

    [deps] Bumped libraries

--- go.mod
+++ go.mod
@@ -1,18 +1,24 @@
 module github.com/burdzwastaken/osquery-s3-config
 
-go 1.20
+go 1.23.0
+
+toolchain go1.24.1
 
 require (
-	github.com/aws/aws-sdk-go v1.44.253
-	github.com/osquery/osquery-go v0.0.0-20230427204930-ff54f5f08296
+	github.com/aws/aws-sdk-go v1.55.7
+	github.com/osquery/osquery-go v0.0.0-20250131154556-629f995b6947
 )
 
 require (
-	github.com/Microsoft/go-winio v0.6.1 // indirect
-	github.com/apache/thrift v0.18.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/apache/thrift v0.22.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/tools v0.8.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
 )
--- pkg/s3/s3.go
+++ pkg/s3/s3.go
@@ -1,9 +1,9 @@
 package s3
 
 import (
-	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -25,13 +25,12 @@ const (
 )
 
 type Plugin struct {
-	bucketName        string
-	bucketRegion      string
-	configurationPath string
-	client            *s3.S3
+	bucketName string
+	configPath string
+	client     *s3.S3
 }
 
-func exitErrorf(msg string, args ...interface{}) {
+func exitErrorf(msg string, args ...any) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 	os.Exit(1)
 }
@@ -53,31 +52,37 @@ func New() *config.Plugin {
 	bucketRegion := getEnv(bucketRegionEnvVar, defaultBucketRegion)
 	configPath := getEnv(bucketConfigPathEnvVar, defaultConfigPath)
 
-	client := s3.New(session.New(), &aws.Config{Region: aws.String(bucketRegion)})
-	plugin := &Plugin{client: client, bucketName: bucketName, configurationPath: configPath}
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(bucketRegion)})
+	if err != nil {
+		exitErrorf("error creating AWS session: %v", err)
+	}
+	client := s3.New(sess)
+	plugin := &Plugin{client: client, bucketName: bucketName, configPath: configPath}
 	return config.NewPlugin(pluginName, plugin.GenerateConfigs)
 }
 
 func (p *Plugin) GenerateConfigs(ctx context.Context) (map[string]string, error) {
-	result, err := p.client.GetObject(&s3.GetObjectInput{
+	input := &s3.GetObjectInput{
 		Bucket: aws.String(p.bucketName),
-		Key:    aws.String(p.configurationPath),
-	})
+		Key:    aws.String(p.configPath),
+	}
+	result, err := p.client.GetObjectWithContext(ctx, input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case s3.ErrCodeNoSuchBucket:
-				exitErrorf("Bucket %s does not exist", p.bucketName)
+				return nil, fmt.Errorf("bucket %s does not exist", p.bucketName)
 			case s3.ErrCodeNoSuchKey:
-				exitErrorf("Object with key %s does not exist in bucket %s", p.configurationPath, p.bucketName)
+				return nil, fmt.Errorf("object with key %s does not exist in bucket %s", p.configPath, p.bucketName)
 			}
 		}
-		exitErrorf("Unknown error occurred, %v", err)
+		return nil, fmt.Errorf("error fetching S3 object: %w", err)
 	}
 	defer result.Body.Close()
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(result.Body)
-
-	return map[string]string{pluginName: buf.String()}, nil
+	data, err := io.ReadAll(result.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading S3 object body: %w", err)
+	}
+	return map[string]string{pluginName: string(data)}, nil
 }
